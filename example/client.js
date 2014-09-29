d12981f6dae289366b396e1a230bc79e6cf22346
Wagner Camarao <wcamarao@circleup.com>
2014-09-29 16:31:27 -0700

    Add support for JSONP

--- example/client.js
+++ example/client.js
@@ -1,12 +1,24 @@
 angular.module('MyApp', ['camelizeHttp']).controller('BodyController', function($scope, $http, cHttp) {
   $scope.data = {};
 
+  // get
+
   $http.get('/data.json').success(function(response) {
-    $scope.data.$http = response;
+    $scope.data.$httpGet = response;
   });
 
   cHttp.get('/data.json').success(function(response) {
-    $scope.data.cHttp = response;
+    $scope.data.cHttpGet = response;
+  });
+
+  // jsonp
+
+  $http.jsonp('/data0.jsonp?callback=JSON_CALLBACK').success(function(response) {
+    $scope.data.$httpJsonp = response;
+  });
+
+  cHttp.jsonp('/data1.jsonp?callback=JSON_CALLBACK').success(function(response) {
+    $scope.data.cHttpJsonp = response;
   });
 
 });
